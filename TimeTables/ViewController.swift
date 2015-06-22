ab0534bc8f353b581937349636e2cdcaa84329c5
Fran Primo Ruiz <FranPrimo@MacBook-de-Fran.local>
2015-06-22 15:04:54 +0200

    comentarios añadidos.

--- TimeTables/ViewController.swift
+++ TimeTables/ViewController.swift
@@ -9,24 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate {
-
+    //Esto nos da el valor de la posicion en la que se encuentra el slider.
     @IBOutlet weak var valueSlider: UISlider!
     
+    //Esto es una instancia de la tabla.
     @IBOutlet weak var table: UITableView!
     
+    //Instancia mediante la cual sé si el usuario ha movido el slider.
     @IBAction func timeSlider(sender: AnyObject) {
         table.reloadData()
         
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        //Este metodo nos devuelve el numero de celdas de la tabla.
         return 20
     }
 
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //Con este metodo rellenamos las celdas de la tabla.
         
+        //Creo una celda a la que le paso el mismo identificador que le he puesto en el diseñador.
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
         
         let timesTable = Int(valueSlider.value * 20)
